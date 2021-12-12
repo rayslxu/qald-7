49d39edec5546e33746356b8982190d7d4e3e2f1
Silei Xu <ray.slxu@gmail.com>
2021-12-12 00:03:44 -0800

    Add a sqlite caching for all http requests

--- lib/wikidata-utils.ts
+++ lib/wikidata-utils.ts
@@ -1,17 +1,76 @@
 import * as Tp from 'thingpedia';
+import * as sqlite3 from 'sqlite3';
+import * as fs from 'fs';
 import { wikibaseSdk } from 'wikibase-sdk'; 
 import wikibase from 'wikibase-sdk';
 
 const URL = 'https://query.wikidata.org/sparql';
 const ENTITY_PREFIX = 'http://www.wikidata.org/entity/';
 
+const SQLITE_SCHEMA = `
+create table cache (
+    url text not null,
+    result text not null
+);`;
+
+interface Cache {
+    result : string
+}
+
 export default class WikidataUtils {
     private _wdk : wikibaseSdk;
-    private _cache : Record<string, any>;
+    private _cache ! : sqlite3.Database;
+    private _cacheLoaded : boolean;
 
     constructor() {
         this._wdk = wikibase({ instance: 'https://www.wikidata.org' });
-        this._cache = {};
+        this._cacheLoaded = false;
+    }
+
+    /**
+     * Load or create sqlite database for caching
+     */
+    private async _loadOrCreateSqliteCache() {
+        const filename = 'wikidata_cache.sqlite';
+        const db = new sqlite3.Database(filename, sqlite3.OPEN_CREATE|sqlite3.OPEN_READWRITE);
+        db.serialize(() => {
+            if (!fs.existsSync(filename)) 
+                db.run(SQLITE_SCHEMA);
+        });
+        this._cache = db;
+    }
+
+    /**
+     * Get cache 
+     * @param url the url of the request
+     * @returns undefined if not found, otherwise in the format of { result : string }
+     */
+    private async _getCache(url : string) : Promise<Cache|undefined> {
+        if (!this._cacheLoaded) 
+            await this._loadOrCreateSqliteCache();
+        return new Promise((resolve, reject) => {
+            const sql = `select result from cache where url = ?`;
+            this._cache.get(sql, url, (err : Error|null, rows : any) => {
+                if (err)
+                    reject(err);
+                else
+                    resolve(rows);
+            });
+        });
+    }
+
+    private async _setCache(url : string, result : string) {
+        if (!this._cacheLoaded) 
+            await this._loadOrCreateSqliteCache();
+        return new Promise((resolve, reject) => {
+            const sql = `insert into cache values (?,?)`;
+            this._cache.get(sql, url, result, (err : Error|null, rows : any) => {
+                if (err)
+                    reject(err);
+                else 
+                    resolve(rows);
+            });
+        });
     }
 
     /**
@@ -20,7 +79,6 @@ export default class WikidataUtils {
      * @returns A list of the results
      */
     private async _query(sparql : string) {
-        console.log(sparql);
         const result = await this._request(`${URL}?query=${encodeURIComponent(sparql)}`);
         return result.results.bindings;
     }
@@ -31,12 +89,13 @@ export default class WikidataUtils {
      * @returns An object of the result
      */
     private async _request(url : string) {
-        if (url in this._cache) 
-            return this._cache[url];
+        const cached = await this._getCache(url);
+        if (cached) 
+            return JSON.parse(cached.result);
         try {
             const result = await Tp.Helpers.Http.get(url, { accept: 'application/json' });
+            await this._setCache(url, result);
             const parsed = JSON.parse(result);
-            this._cache[url] = parsed;
             return parsed;
         } catch(e) {
             console.log(e);
