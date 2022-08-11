d259478b22d067cadcc444ad7be3e32c0ed0efe7
Silei Xu <ray.slxu@gmail.com>
2022-08-11 13:35:52 -0700

    Add python scripts for result analysis

---
+++ data/compare-results.py
@@ -0,0 +1,21 @@
+import sys
+
+path1 = './experiments/' + sys.argv[1] + '/test.debug'
+path2 = './experiments/' + sys.argv[2] + '/test.debug'
+
+failed_example1 = []
+with open(path1, 'r') as f1:
+    for line in f1:
+        id = line.split('\t')[0]
+        failed_example1.append(id)
+
+failed_example2 = []
+with open(path2, 'r') as f2:
+    for line in f2:
+        id = line.split('\t')[0]
+        failed_example2.append(id)
+
+
+for id in failed_example2:
+    if id not in failed_example1:
+        print(id)
