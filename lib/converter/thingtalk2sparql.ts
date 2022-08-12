9ce23b2773954e76e56230b7b9391fea206c8d74
Silei Xu <ray.slxu@gmail.com>
2022-08-11 17:01:17 -0700

    Convert aggregation filter

--- lib/converter/thingtalk2sparql.ts
+++ lib/converter/thingtalk2sparql.ts
@@ -9,8 +9,14 @@ const ENTITY_VARIABLES = ['x', 'y', 'z'];
 // const PREDICATE_VARIABLES = ['p', 'q', 'r'];
 
 class TableInfoVisitor extends Ast.NodeVisitor {
+    private _converter : ThingTalkToSPARQLConverter;
     subject ?: string;
     domainName ?: string;
+
+    constructor(converter : ThingTalkToSPARQLConverter) {
+        super();
+        this._converter = converter;
+    }
 
     visitChainExpression(node : ThingTalk.Ast.ChainExpression) : boolean {
         if (node.expressions.length > 1)   
@@ -25,42 +31,30 @@ class TableInfoVisitor extends Ast.NodeVisitor {
             this.domainName = node.value.value!;
         return true;
     }
+
+    visitInvocation(node : ThingTalk.Ast.Invocation) : boolean {
+        if (this._converter.humanReadableInstanceOf) {
+            this.domainName = node.channel.replace(/_/g, ' ');
+        } else {
+            const query = this._converter.class.getFunction('query', node.channel);
+            this.domainName = (query?.getImplementationAnnotation('wikidata_subject') as string[])[0];
+        } 
+        return true;
+    }
 }
 
 class TripleGenerator extends Ast.NodeVisitor {
     private _converter : ThingTalkToSPARQLConverter;
     private _subject : string;
-    private _isBooleanQuestion : boolean;
     private _target_projection : string|null;
-    private _target_projection_variable : string|null;
-    private _triples : string[];
 
     constructor(converter : ThingTalkToSPARQLConverter, subject : string, projection : string|null, domain : string|null) {
         super();
         this._converter = converter;
         this._subject = subject;
-        this._isBooleanQuestion = false;
         this._target_projection = projection;
-        this._target_projection_variable = null;
-        this._triples = [];
-        if (domain)
-            this._triples.push(`${subject} <${PROPERTY_PREFIX}P31> <${ENTITY_PREFIX}${domain}>.`);
-    }
-
-    get triples() {
-        return this._triples;
-    }
-
-    get resultVariable() {
-        if (this._target_projection_variable)
-            return this._target_projection_variable;
-        if (this._subject.startsWith('?'))
-            return this._subject;
-        return null;
-    }
-
-    get isBooleanQuestion() {
-        return this._isBooleanQuestion;
+        if (subject.startsWith('?') && domain)
+            this._converter.addStatement(`${subject} <${PROPERTY_PREFIX}P31> <${ENTITY_PREFIX}${domain}>.`);
     }
 
     visitProjectionExpression(node : ThingTalk.Ast.ProjectionExpression) : boolean {
@@ -68,8 +62,8 @@ class TripleGenerator extends Ast.NodeVisitor {
             const p = this._converter.getWikidataProperty(arg);
             const v = this._converter.getEntityVariable();
             if (arg === this._target_projection) 
-                this._target_projection_variable = `?${v}`;
-            this._triples.push(`${this._subject} <${PROPERTY_PREFIX}${p}> ?${v}.`);
+                this._converter.setResultVariable(`?${v}`);
+            this._converter.addStatement(`${this._subject} <${PROPERTY_PREFIX}${p}> ?${v}.`);
         }
         return true;
     }
@@ -86,18 +80,18 @@ class TripleGenerator extends Ast.NodeVisitor {
                     path.push(`<${PROPERTY_PREFIX}${property}>${element.quantifier ?? ''}`);
                 }
                 const v = this._converter.getEntityVariable();
-                this._triples.push(`${this._subject} ${path.join('/')}, ?${v}.`);
+                this._converter.addStatement(`${this._subject} ${path.join('/')}, ?${v}.`);
             } else {
                 const p = this._converter.getWikidataProperty(proj.value);
                 const v = this._converter.getEntityVariable();
-                this._triples.push(`${this._subject} <${PROPERTY_PREFIX}${p}> ?${v}.`);
+                this._converter.addStatement(`${this._subject} <${PROPERTY_PREFIX}${p}> ?${v}.`);
             }     
         }
         return true;
     }
 
     visitBooleanQuestionExpression(node : ThingTalk.Ast.BooleanQuestionExpression) : boolean {
-        this._isBooleanQuestion = true;
+        this._converter.setIsBooleanQuestion();
         return true;
     }
 
@@ -106,10 +100,26 @@ class TripleGenerator extends Ast.NodeVisitor {
             const property = node.name;
             const p = this._converter.getWikidataProperty(property);
             const v = (node.value as Ast.EntityValue).value!;
-            this._triples.push(`${this._subject} <${PROPERTY_PREFIX}${p}> <${ENTITY_PREFIX}${v}>.`);
+            this._converter.addStatement(`${this._subject} <${PROPERTY_PREFIX}${p}> <${ENTITY_PREFIX}${v}>.`);
 
         }
         return true;
+    }
+
+    visitComputeBooleanExpression(node : ThingTalk.Ast.ComputeBooleanExpression) : boolean {
+        if (node.lhs instanceof Ast.Value.Computation) {
+            if (node.lhs.op === 'count') {
+                const property = (node.lhs.operands[0] as Ast.VarRefValue).name;
+                const p = this._converter.getWikidataProperty(property);
+                const op = ['>=', '<='].includes(node.operator) ? node.operator[0] : node.operator; // HACK
+                const value = (node.rhs as Ast.NumberValue).value;
+                const variable = this._converter.getEntityVariable();
+                this._converter.addStatement(`${this._subject} <${PROPERTY_PREFIX}${p}> ?${variable}.`);
+                this._converter.addHaving(`COUNT(?${variable}) ${op} ${value}`);
+                return true;
+            }
+        }
+        throw new Error('Unsupported compute boolean expression: ' + node.prettyprint());
     }
 }
 
@@ -138,6 +148,7 @@ export default class ThingTalkToSPARQLConverter {
     private _resultVariable : string|null;
     private _isBooleanQuestion : boolean;
     private _statements : string[];
+    private _having : string[];
     private _humanReadableInstanceOf : boolean;
 
     constructor(classDef : Ast.ClassDef, entities : Entity[], options : ThingTalkToSPARQLConverterOptions) {
@@ -161,13 +172,22 @@ export default class ThingTalkToSPARQLConverter {
 
         this._entityVariableCount = 0;
         this._statements = [];
+        this._having = [];
         this._resultVariable = null;
         this._isBooleanQuestion = false;
         this._humanReadableInstanceOf = options.human_readable_instance_of;
     }
 
+    get class() {
+        return this._classDef;
+    }
+
     get kb() {
         return this._kb;
+    }
+
+    get humanReadableInstanceOf() {
+        return this._humanReadableInstanceOf;
     }
 
     getEntityVariable() : string {
@@ -187,6 +207,22 @@ export default class ThingTalkToSPARQLConverter {
         return domain;
     }
 
+    addStatement(statement : string) {
+        this._statements.push(statement);
+    }
+
+    addHaving(having : string) {
+        this._having.push(having);
+    }
+
+    setIsBooleanQuestion() {
+        this._isBooleanQuestion = true;
+    }
+
+    setResultVariable(variable : string) {
+        this._resultVariable = variable;
+    }
+
     private _reset() {
         this._entityVariableCount = 0;
         this._statements = [];
@@ -195,16 +231,15 @@ export default class ThingTalkToSPARQLConverter {
     }
 
     private async _convertSingleTable(ast : Ast.Expression) {
-        const tableInfoVisitor = new TableInfoVisitor();
+        const tableInfoVisitor = new TableInfoVisitor(this);
         ast.visit(tableInfoVisitor);
         const subject = tableInfoVisitor.subject ?? '?' + this.getEntityVariable();
+        if (subject.startsWith('?'))
+            this.setResultVariable(subject);
         const projection = ast instanceof Ast.ProjectionExpression ? ast.args[0] : null;
         const domain = tableInfoVisitor.domainName ? await this.getWikidataDomain(tableInfoVisitor.domainName) : null;
         const tripleGenerator = new TripleGenerator(this, subject, projection, domain);
         ast.visit(tripleGenerator);
-        this._statements.push(...tripleGenerator.triples);
-        this._resultVariable = tripleGenerator.resultVariable;
-        this._isBooleanQuestion = tripleGenerator.isBooleanQuestion;
     }
 
     async convert(utterance : string, thingtalk : string) : Promise<string> {
@@ -218,8 +253,10 @@ export default class ThingTalkToSPARQLConverter {
         assert(expr instanceof Ast.ChainExpression && expr.expressions.length === 1);
         const table = expr.expressions[0];
         await this._convertSingleTable(table);  
-        return this._isBooleanQuestion ? 
-            `ASK WHERE { ${this._statements.join((' '))} }` : 
-            `SELECT DISTINCT ${this._resultVariable} WHERE { ${this._statements.join((' '))} }`;
+        const sparql = (this._isBooleanQuestion ? `ASK ` : `SELECT DISTINCT ${this._resultVariable} `) + 
+            `WHERE { ${this._statements.join((' '))} }`;
+        if (this._having.length > 0)
+            return sparql + ` GROUP BY ${this._resultVariable} HAVING(${this._having.join(' && ')})`;
+        return sparql;
     }
 }
