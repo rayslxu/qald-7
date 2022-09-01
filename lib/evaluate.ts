0b36cb10f793a60917426e4fbdf7d4f9a78523ef
Silei Xu <ray.slxu@gmail.com>
2022-09-01 11:12:49 -0700

    Evaluate: add the option to rerun sparql, or load from thingtalk

--- lib/evaluate.ts
+++ lib/evaluate.ts
@@ -1,10 +1,12 @@
 import assert from 'assert';
 import * as fs from 'fs';
 import * as stream from 'stream';
+import csvparse from 'csv-parse';
 import JSONStream from 'JSONStream';
 import * as argparse from 'argparse';
 import { waitFinish } from './utils/misc';
 import WikidataUtils from './utils/wikidata';
+import { StreamUtils } from 'genie-toolkit';
 
 async function main() {
     const parser = new argparse.ArgumentParser({
@@ -15,22 +17,62 @@ async function main() {
         required: true,
         type: fs.createReadStream
     });
+    parser.add_argument('--reevaluate', {
+        action: 'store_true',
+        default: false,
+        help: 're-evaluate original SPARQL and update answers'
+    });
+    parser.add_argument('--from-thingtalk', {
+        action: 'store_true',
+        default: false,
+        help: 'use converted thingtalk file instead of the original raw file'
+    });
     parser.add_argument('--prediction', {
         required: true
     });
+    parser.add_argument('--cache', {
+        required: false,
+        default: 'wikidata_cache.sqlite'
+    });
+    parser.add_argument('--save-cache', {
+        action: 'store_true',
+        default: false
+    });
+    parser.add_argument('--bootleg-db', {
+        required: false,
+        default: 'bootleg.sqlite'
+    });
     const args = parser.parse_args();
-    const dataset = args.dataset.pipe(JSONStream.parse('questions.*')).pipe(new stream.PassThrough({ objectMode: true }));
+    const wikidata = new WikidataUtils(args.cache, args.bootleg_db, args.save_cache);
     const expectedAnswer : Record<string, string[]> = {};
-    for await (const item of dataset) {
-        assert(item.answers.length === 1);
-        const answer = item.answers[0];
-        expectedAnswer[item.id] = WikidataUtils.processRawResult(answer);
+    let dataset;
+    if (args.from_thingtalk) {
+        const columns = ['id', 'sentence', 'answers', 'sparql'];
+        dataset = args.dataset.pipe(csvparse({ columns, delimiter: '\t', relax: true }))
+            .pipe(new StreamUtils.MapAccumulator());
+        const data = await dataset.read(); 
+        for (const ex of data.values()) 
+            expectedAnswer[ex.id] = await wikidata.query(ex.sparql);
+    } else {
+        dataset = args.dataset.pipe(JSONStream.parse('questions.*')).pipe(new stream.PassThrough({ objectMode: true }));
+        for await (const item of dataset) {
+            if (args.reevaluate) {
+                const sparql = item.query.sparql;
+                expectedAnswer[item.id] = await wikidata.query(sparql);
+            } else {
+                assert(item.answers.length === 1);
+                const answer = item.answers[0];
+                expectedAnswer[item.id] = WikidataUtils.processRawResult(answer);
+            }
+        }
     }
 
     const predictions = fs.readFileSync(args.prediction, { encoding: 'utf8' }).split('\n');
     const MicroF1s = [];
     let [MacroTP, MacroFP, MacroFN] = [0, 0, 0];
     for (const line of predictions) {
+        if (line.trim().length === 0)
+            continue;
         const [id, , answers, ] = line.split('\t');
         const expected = expectedAnswer[id];
         const predicted = answers.split(' ');
@@ -50,6 +92,7 @@ async function main() {
     console.log('Macro F1: ', MacroF1);
 
     await waitFinish(dataset);
+
 }
 
 if (require.main === module)
