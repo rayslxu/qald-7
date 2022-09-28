0aaee638ded648a464d19b3dc811919737c9ec82
Silei Xu <ray.slxu@gmail.com>
2022-09-28 11:07:39 -0700

    Update error analysis script to download data from azure

--- data/sync-dataset.sh
+++ data/sync-dataset.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 # args: s3-dir dataset-name
-aws s3 sync $1 experiments/$2 --exclude "chunked/*"
+# eg.: ./sync-dataset.sh silei/dataset/qald/qald7/24/1664272755/ 24
+
+bucket=https://nfs009a5d03c43b4e7e8ec2.blob.core.windows.net/pvc-a8853620-9ac7-4885-a30e-0ec357f17bb6
+azcopy sync ${bucket}/$1 experiments/$2 --exclude-path "chunked/"
--- data/sync-result.sh
+++ data/sync-result.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 # args: eval/test id model-name
-aws s3 cp s3://geniehai/silei/workdir/qald/$1/silei/$2/$3.results experiments/$3/$1.results
-aws s3 cp s3://geniehai/silei/workdir/qald/$1/silei/$2/$3.debug experiments/$3/$1.debug
+# e.g., `./sync-result.sh test 1664299068 24`
+
+bucket=https://nfs009a5d03c43b4e7e8ec2.blob.core.windows.net/pvc-a8853620-9ac7-4885-a30e-0ec357f17bb6
+azcopy cp ${bucket}/silei/workdir/qald/$1/silei/$2/$3.results experiments/$3/$1.results
+azcopy cp ${bucket}/silei/workdir/qald/$1/silei/$2/$3.debug experiments/$3/$1.debug
