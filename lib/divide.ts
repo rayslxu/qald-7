a72bf639e4a3e904c5c7da34b09e07ed7c517483
Silei Xu <ray.slxu@gmail.com>
2022-11-08 16:57:46 -0800

    Add the option to set size when split dataset

--- lib/divide.ts
+++ lib/divide.ts
@@ -9,15 +9,21 @@ async function main() {
     parser.add_argument('input', {
         help: "the file to be divided"
     });
+    parser.add_argument('--size', {
+        required: false,
+        help: "size of the second slice of the data",
+        type: parseInt
+    });
 
     const args = parser.parse_args();
     const data = JSON.parse(fs.readFileSync(args.input, { encoding: 'utf8' }));
     const length = data.questions.length;
+    const size = args.size ? length - args.size : length/2;
     const a = {
-        questions: data.questions.slice(0, length/2)
+        questions: data.questions.slice(0, size)
     };
     const b = {
-        questions: data.questions.slice(length/2)
+        questions: data.questions.slice(size)
     };
     fs.writeFileSync('xaa', JSON.stringify(a, undefined, 2));
     fs.writeFileSync('xab', JSON.stringify(b, undefined, 2));
