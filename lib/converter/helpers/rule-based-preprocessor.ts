767ad407e2c2b14f61f37b0fd02a1762c98a5a0a
Silei Xu <ray.slxu@gmail.com>
2023-02-18 00:17:42 -0800

    Add rule-based preprocess before conversion; add rule to convert government questions

---
+++ lib/converter/helpers/rule-based-preprocessor.ts
@@ -0,0 +1,101 @@
+import WikidataUtils from '../../utils/wikidata';
+import { 
+    ENTITY_PREFIX,
+    PROPERTY_PREFIX,
+    PROPERTY_PREDICATE_PREFIX,
+    PROPERTY_QUALIFIER_PREFIX,
+    PROPERTY_STATEMENT_PREFIX
+} from '../../utils/wikidata';
+
+const prefixes : Record<string, string> = {
+    'wd': ENTITY_PREFIX,
+    'wdt': PROPERTY_PREFIX,
+    'p': PROPERTY_PREDICATE_PREFIX,
+    'pq': PROPERTY_QUALIFIER_PREFIX,
+    'ps': PROPERTY_STATEMENT_PREFIX
+};
+
+export class RuleBasedPreprocessor {
+    private _wikidata : WikidataUtils;
+
+    constructor(wikidata : WikidataUtils) {
+        this._wikidata = wikidata;
+    }
+
+    private _normalize(sparql : string) : string {
+        const regex = new RegExp('(wd|wdt|p|pq|ps):([P|Q][0-9]+)', 'g');
+        for (const [abbr, prefix] of Object.entries(prefixes)) {
+            sparql = sparql.replace(`PREFIX ${abbr}: <${prefix}>`, '');
+            let match;
+            while ((match = regex.exec(sparql)) !== null) {
+                const abbr = match[1];
+                const id = match[2];
+                sparql = sparql.replace(`${abbr}:${id}`, `<${prefixes[abbr]}${id}>`);
+            }
+        }
+        return sparql.replace(/\s+/g, ' ').trim();
+    }
+
+    async _leader(code : string, type : 'thingtalk'|'sparql') : Promise<string|null> {
+        if (type === 'thingtalk') {
+            const regex = new RegExp('\\[ (head_of_government|head_of_state) filter \\( point_in_time == new Date \\( ([0-9]+) \\) \\) \\] of @wd . (country|entity) \\( \\) filter id == " (Q[0-9]+) "');
+            const match = regex.exec(code);
+            if (!match)
+                return null;
+            const year = match[2];
+            const country = match[4];
+            const office_held_by_property = match[1] === 'head_of_government' ? 'P1313' : 'P1906';
+            const result = await this._wikidata.query(`SELECT DISTINCT ?x WHERE {
+                wd:${country} wdt:${office_held_by_property} ?x.
+            }`);
+            if (result.length === 0) {
+                console.warn(`Cannot find entity for ${country}'s ${office_held_by_property}`);
+                return null;
+            }
+            const position = result[0];
+            return `@wd . entity ( ) filter contains ( position_held filter ( point_in_time == new Date ( ${year} ) ) , " ${position} " ^^wd:p_position_held ) ;`;
+        } else {
+            const normalized = this._normalize(code);
+            const pRegex = new RegExp(`\\?([x|y|z]) <http://www\\.wikidata\\.org/prop/P39> \\?([p|q])`);
+            const pMatch = pRegex.exec(normalized);
+            if (!pMatch)
+                return null;
+            const xVariable = pMatch[1];
+            const pVariable = pMatch[2];
+
+            const psRegex = new RegExp(this._normalize(`\\?${pVariable} <http://www\\.wikidata\\.org/prop/statement/P39> <http://www\\.wikidata\\.org/entity/(Q[0-9]+)>`));
+            const psMatch = psRegex.exec(normalized);
+            if (!psMatch)
+                return null;
+            const position_held = psMatch[1];
+            const office_held_by : Record<string, string> = {
+                'P1313': 'P6',
+                'P1906': 'P35'
+            };
+            let country, held_by;
+            for (const office in office_held_by) {
+                const result = await this._wikidata.query(`SELECT DISTINCT ?x WHERE {
+                    ?x wdt:${office} wd:${position_held}.
+                }`);
+                if (result.length > 0) {
+                    country = result[0];
+                    held_by = office_held_by[office];
+                    break;
+                }
+            }
+            if (!country)
+                return null;
+            
+            return normalized
+                .replace(pRegex, `<http://www.wikidata.org/entity/${country}> <http://www.wikidata.org/prop/${held_by}> ?${pVariable}`)
+                .replace(psRegex, `?${pVariable} <http://www.wikidata.org/prop/statement/${held_by}> ?${xVariable}`);
+        }
+    }
+
+    async preprocess(code : string, type : 'thingtalk'|'sparql') : Promise<string> {
+        let processed : string|null;
+        if ((processed = await this._leader(code, type)) !== null)
+            return processed;
+        return code;
+    }
+}  
--- lib/converter/thingtalk2sparql.ts
+++ lib/converter/thingtalk2sparql.ts
@@ -14,6 +14,7 @@ import {
     PROPERTY_STATEMENT_PREFIX
 } from '../utils/wikidata';
 import { PatternConverter } from './helpers/pattern-convertor';
+import { RuleBasedPreprocessor } from './helpers/rule-based-preprocessor';
 
 const ENTITY_VARIABLES = ['x', 'y', 'z'];
 const PREDICATE_VARIABLES = ['p', 'q', 'r'];
@@ -517,6 +518,7 @@ export default class ThingTalkToSPARQLConverter {
     private _locale : string;
     private _timezone ?: string;
     private _kb : WikidataUtils;
+    private _preprocessor : RuleBasedPreprocessor;
     private _patternConverter : PatternConverter;
     private _propertyMap : Record<string, string>;
     private _domainMap : Record<string, string>;
@@ -540,6 +542,7 @@ export default class ThingTalkToSPARQLConverter {
         this._timezone = options.timezone;
 
         this._kb = new WikidataUtils(options.cache, options.bootleg, options.save_cache);
+        this._preprocessor = new RuleBasedPreprocessor(this._kb);
         this._patternConverter = new PatternConverter();
         this._propertyMap = { "P31" : "instance_of" };
         for (const property of this._classDef.queries['entity'].iterateArguments()) {
@@ -704,6 +707,9 @@ export default class ThingTalkToSPARQLConverter {
 
     async convert(utterance : string, thingtalk : string) : Promise<string> {
         this._reset();
+
+        // preprocess
+        thingtalk = await this._preprocessor.preprocess(thingtalk, 'thingtalk');
 
         // try pattern match first
         const patternConverterResult = this._patternConverter.toSPARQL(thingtalk);
