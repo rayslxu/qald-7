604517ceb98a8b22e6f156c540583a7b675e6e31
Jake Wu <jmhw0123@gmail.com>
2023-04-18 12:37:00 +0800

    update pattern

--- lib/converter/helpers/pattern-convertor.ts
+++ lib/converter/helpers/pattern-convertor.ts
@@ -357,6 +357,17 @@ const patterns = {
         ?p <http://www.wikidata.org/prop/direct/P179> <http://www.wikidata.org/entity/$0>; <http://www.wikidata.org/prop/direct/P527> ?x. 
         ?x <http://www.wikidata.org/prop/P161> ?z. 
         ?z <http://www.wikidata.org/prop/statement/P161> <http://www.wikidata.org/entity/$1>. 
+    }`,
+
+    // who are the X state representatives
+    '@wd . entity ( ) filter contains ( position_held filter contains ( < electoral_district / located_in_the_administrative_territorial_entity > , " $0 " ^^wd:entity ) , " Q13218630 " ^^wd:entity ) ;':
+    `PREFIX wd:  PREFIX wdt:  PREFIX p:  PREFIX ps:  PREFIX pq: 
+    SELECT DISTINCT ?x WHERE { 
+        ?x <http://www.wikidata.org/prop/P39> ?p. 
+        ?p <http://www.wikidata.org/prop/statement/P39> <http://www.wikidata.org/entity/Q13218630>. 
+        ?p <http://www.wikidata.org/prop/qualifier/P768> ?y. 
+        ?y <http://www.wikidata.org/prop/direct/P131> <http://www.wikidata.org/entity/$0>. 
+        FILTER NOT EXISTS { ?p <http://www.wikidata.org/prop/qualifier/P582> ?z. } 
     }`
 };
 
