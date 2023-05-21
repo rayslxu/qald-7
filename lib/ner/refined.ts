bb4e97ab004bff5daa6676e4b00310a7d3972c3e
Silei Xu <ray.slxu@gmail.com>
2023-05-20 17:34:12 -0700

    Split file in to batches to run Refined

--- lib/ner/refined.ts
+++ lib/ner/refined.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import path from 'path';
 import * as child_process from 'child_process';
 import { Example, Linker, LinkerResult } from './base';
 import WikidataUtils from '../utils/wikidata';
@@ -19,15 +21,25 @@ export class ReFinEDLinker extends Linker {
     }
 
     async saferunAll(examples : Example[]) {
-        const process = child_process.spawn('python', [`${__dirname}/../../../python/run_refined.py`, this._path, '--model', this._model], { stdio: ['pipe', 'pipe', 'inherit'] });
-        const stdout : string = await new Promise((resolve, reject) => {
-            process.on('error', reject);
-            process.stdout.setEncoding('utf8');
-            let buffer = '';
-            process.stdout.on('data', (data) => buffer += data);
-            process.stdout.on('end', () => resolve(buffer));
-        });
-        const entitiesByExample = JSON.parse(stdout);
+        const entitiesByExample : Record<string, any> = {};
+        for (const f of fs.readdirSync(path.dirname(this._path))) {
+            if (!f.startsWith(path.basename(this._path) + '-split'))
+                continue;
+            try {
+                const p = path.join(path.dirname(this._path), f);
+                const process = child_process.spawn('python', [`${__dirname}/../../../python/run_refined.py`, p, '--model', this._model], { stdio: ['pipe', 'pipe', 'inherit'] });
+                const stdout : string = await new Promise((resolve, reject) => {
+                    process.on('error', reject);
+                    process.stdout.setEncoding('utf8');
+                    let buffer = '';
+                    process.stdout.on('data', (data) => buffer += data);
+                    process.stdout.on('end', () => resolve(buffer));
+                });
+                Object.assign(entitiesByExample, JSON.parse(stdout));
+            } catch(e) {
+                console.log(e);
+            }
+        }
 
         for (const example of examples) {
             if (example.entities === undefined)
